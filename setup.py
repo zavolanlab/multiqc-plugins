9a2111e8079ae14aa4e111020085513d64ec0a10
Maciek Bąk <wsciekly.maciek@gmail.com>
2021-03-19 19:16:53 +0100

    feat: zpca plugin included (#21)
    * Merge pull request #8 from zavolanlab/dev (#9)
    
    * docs: cleared text in the report for tin score section (#11)
    
    * build: fix text in the report
    
    * style: add flake8 line exception
    
    * style: add flake8 line exception
    
    * Add Dockerfile and github action for docker hub (#14)
    
    * Add Dockerfile and github action that automatically builds and pushes an image on Dockerhub
    
    * Fix indentation. Rename github action job name.
    
    * build: Export C.UTF-8 in Dockerfile (#16)
    
    * feat: add zpca plugin (#19)
    
    * feat: add zpca plugin
    
    * Tests: Update Build Tests
    
    * Tests: Update Lint tests
    
    * info : add zpca
    
    * restore file
    
    * feat: add scree plots
    
    * Docs: add and edit links
    
    * make final changes
    
    * refactor: labels
    
    * minor: make title lowercase
    
    * Update report
    
    * minor: add percentage sumbol
    
    * docs: version 1.1.0 bump (#20)
    
    Co-authored-by: Foivos Gypas <fgypas@users.noreply.github.com>
    Co-authored-by: Krish Agarwal <akrish136@gmail.com>

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="multiqc_plugins",
-    version="0.0.1",
+    version="1.1.0",
     author="Krish Agarwal",
     author_email="akrish136@gmail.com",
     description="MultiQC plugins for the Zavolan Lab \
@@ -10,7 +10,7 @@ setup(
     long_description=__doc__,
     keywords="bioinformatics",
     url="https://github.com/zavolanlab/multiqc-plugins",
-    download_url="",  # after releasing a version
+    download_url="https://github.com/zavolanlab/multiqc-plugins",
     license="Apache 2.0",
     packages=find_packages(),
     include_package_data=True,
@@ -19,6 +19,7 @@ setup(
         "multiqc.modules.v1": [
             "ALFA = modules.ALFA:MultiqcModule",
             "tin-score = modules.tin_score:MultiqcModule",
+            "zpca = modules.zpca:MultiqcModule",
         ],
         "multiqc.hooks.v1": ["execution_start = modules.hook:execution_start"],
     },
